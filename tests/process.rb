c23e2ffebaa78a8075c85ca899cc590b069e1d7f
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-07-01 12:40:43 +0100

    add `tests/process.rb`

---
+++ tests/process.rb
@@ -0,0 +1,61 @@
+require "minitest/autorun"
+
+require_relative "../squarkdown/core/process"
+
+
+class Process < Minitest::Test
+
+  def test_live
+    content = """# Testing
+<!-- #SQUARK live! -->"""
+
+    data = extract_data(content.split)
+    assert data["live"] == true
+  end
+
+  def test_dead
+    content = """# Testing
+<!-- #SQUARK dead! -->"""
+
+    data = extract_data(content.split "\n")
+    assert data["live"] == false
+  end
+
+  def test_fields
+    content = """# Testing
+<!-- #SQUARK live!
+| title = Squarkdown is awesome
+| dest = testing/fields
+| style = #auto / test
+| duality = dark
+| date = 1 April 1984
+| index = tests
+| shard = #index / testing
+-->"""
+
+    data = extract_data(content.split "\n")
+    assert data["title"] == "Squarkdown is awesome"
+    assert data["dest"] == "testing/fields"
+    assert data["style"] == ["essence", "test"]
+    assert data["duality"] == "dark"
+    assert data["index"] == ["tests"]
+    assert data["shard"] == ["tests", "testing"]
+  end
+
+  def test_fields_default
+    content = """# Testing
+<!-- #SQUARK live!
+| title = Squarkdown is cool
+| dest = testing/defaults
+-->"""
+
+    data = extract_data(content.split "\n")
+    assert data["title"] == "Squarkdown is cool"
+    assert data["dest"] == "testing/defaults"
+    assert data["style"] == ["essence"]
+    assert data["duality"] == "light"
+    assert data["index"] == []
+    assert data["shard"] == []
+  end
+
+end
