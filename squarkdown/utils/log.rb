95385913d7300dfe2cf283be1f369a3a9bceed1b
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-07-02 10:27:10 +0000

    develop `utils/log.rb`

--- squarkdown/utils/log.rb
+++ squarkdown/utils/log.rb
@@ -1,12 +1,16 @@
-started = false
+$started = false
 
 
-def log(action)
-  if !started
+def log(action = nil, error: nil, done: false)
+  if done
+    puts ">>> Ruby / done!"
+  elsif error
+    puts "-------- / #{error}"
+  elsif !$started
     puts ">>> Ruby / #{action}"
   else
     puts "        / #{action}"
   end
 
-  started = true
+  $started = true
 end
