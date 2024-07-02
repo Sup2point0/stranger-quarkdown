cb51edde98a1d6a5a600d554c5c888fe567e282a
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-07-02 09:52:38 +0100

    develop `tests/render.rb`

--- tests/render.rb
+++ tests/render.rb
@@ -13,4 +13,37 @@ class SquarkupRender < Minitest::Test
     "repo" => "Squarkdown Tests"
   }
 
+  
+  def test_head
+    content = """# Testing
+    """
+
+    out = inject_head(content, data: Data, repo_config: RepoConfig)
+
+    assert out.include?("<title> Squarkdown is epic · Squarkdown Tests </title>")
+  end
+
+
+  def test_repl
+    content = """# Testing
+
+    <!-- #SQUARK leave? -->
+    unprocessed content
+    <!-- #SQUARK leave. -->
+
+    <!-- #SQUARK only?
+    processed content
+         #SQUARK only. -->
+
+    <!-- #SQUARK testing! -->
+    """
+
+    out = inject_repl(content, data: Data, repo_config: RepoConfig)
+
+    assert !out.include?("unprocessed content")
+    assert out.include?("processed content")
+    assert !out.include?("#SQUARK only")
+    assert !out.include?("#SQUARK testing")
+  end
+
 end
