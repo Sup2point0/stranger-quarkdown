b7ec0975bb2ae2077e861a8f57b86d84de979f62
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-07-04 12:28:22 +0000

    add colours to `utils/log`

--- squarkdown/utils/log.rb
+++ squarkdown/utils/log.rb
@@ -1,16 +1,26 @@
 $started = false
 
 
+Cols = {
+  white: "\033[38;2;255;255;255m",
+  grey: "\033[38;2;169;176;184m",
+  pink: "\033[38;2;255;0;144m",
+  lilac: "\033[38;2;199;199;255m",
+}
+
+
 def log(action = nil, error: nil, done: false)
   if done
-    puts ">>> Ruby / done!"
+    puts "#{Cols[:grey]}>>> #{Cols[:pink]}Ruby #{Cols[:grey]}/ #{Cols[:lilac]}done!"
   elsif error
-    puts "-------- / #{error}"
+    puts "#{Cols[:grey]}-------- / #{Cols[:pink]}#{error}"
   elsif !$started
-    puts ">>> Ruby / #{action}"
+    puts "#{Cols[:grey]}>>> #{Cols[:pink]}Ruby #{Cols[:grey]}/ #{Cols[:lilac]}#{action}"
   else
-    puts "        / #{action}"
+    puts "        #{Cols[:grey]}/ #{Cols[:lilac]}#{action}"
   end
+
+  puts Cols[:white]
 
   $started = true
 end
