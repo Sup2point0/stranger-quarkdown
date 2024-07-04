70b094d937c825c9e6104de9c5a6029bb44db754
Sup#2.0 <102817779+Sup2point0@users.noreply.github.com>
2024-07-04 14:58:14 +0100

    add `prep-scss.rb`

---
+++ squarkdown/scripts/prep-scss.rb
@@ -0,0 +1,39 @@
+require_relative "../config"
+require_relative "../utils/log"
+
+
+def prep_scss(repo_config:)
+  log "preprocessing assets..."
+
+  begin
+    try_prep_scss(repo_config:)
+  rescue => e
+    log error: e.to_s
+  end
+end
+
+
+def try_prep_scss(repo_config:)
+  partial = repo_config["paths"]["styles"]
+  route = SITE / partial
+  raise "styles path not found" unless route
+
+  files = route.glob("**/~*.scss")
+  data = files.map do |sheet|
+    dest = file.relative_path_from(route)
+    "@use './#{partial}/#{dest}' as *;"
+  end
+
+  data = data.join("\n")
+
+  text = """/// SCSS Config
+/// Last generated #{}
+
+const scssConfig = `
+#{data.join("\n")}
+`;
+export default scssConfig;
+"""
+
+  File.write(SITE / "scss-config.js", text)
+end
