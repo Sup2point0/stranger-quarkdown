a7c38f650d6887938d72e4a73192eb552f464606
Sup#2.0 <sup2point0@gmail.com>
2024-07-13 16:34:22 +0100

    add `utils/vars.rb` with `Vars` mixin

---
+++ squarkdown/utils/vars.rb
@@ -0,0 +1,25 @@
+## Implements the `Vars` mixin for fetching the instance variables of an object as a hash.
+
+module Vars
+  def vars_str
+    Hash[
+      self.instance_variables.map { |var|
+        [
+          var.to_s.delete("@").to_sym,
+          instance_variable_get(var)
+        ]
+      }
+    ]
+  end
+  
+  def vars_sym
+    Hash[
+      self.instance_variables.map { |var|
+        [
+          var.to_s.delete("@"),
+          instance_variable_get(var)
+        ]
+      }
+    ]
+  end
+end
