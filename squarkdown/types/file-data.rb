5c36c5c428956bb5c62cdac977afc8b6211d640f
Sup#2.0 <sup2point0@gmail.com>
2024-07-13 17:01:47 +0100

    develop `FileData`

--- squarkdown/types/file-data.rb
+++ squarkdown/types/file-data.rb
@@ -1,20 +1,21 @@
 require "json"
+require "yaml"
 
 require_relative "../config"
 require_relative "../utils/vars"
 
 
-class ValidationError < StandardError
-end
-
-
 class FileData
   include Vars
+  
+  class ValidationError < StandardError
+  end
   
   # sentinel for unset required fields
   Unset = Object.new
 
-  attr_reader :path, :live, :isIndex, :isFeatured, :isWoozy, :title, :head, :capt, :desc, :style, :duality, :index, :shard, :date, :date_display, :clean,
+
+  attr_reader :path, :live, :isIndex, :isFeatured, :isWoozy, :title, :head, :capt, :desc, :style, :duality, :index, :shard, :date, :date_display, :clean
 
   Fields = [:dest, :title, :capt, :desc, :style, :duality, :index, :shard, :date, :clean]
 
@@ -51,41 +52,71 @@ class FileData
     end
   end
   
+
   def _parse_(field:, value:, repo_config:)
     case field
 
-    when "title"
-      @title = value
+    when :dest then @dest = value
+    when :title then @title = value
+    when :capt then @capt = value
+    when :desc then @desc = value
 
-    when "head"
-      @head = value[2..]
+    when :duality then @duality = value.downcase
+    when :index then @index = _split_(value)
+    when :shard then @shard = _split_(value)
+    when :clean then @clean = _split_(value)
 
-    when "style"
-      @style = value.downcase.split(" / ").unshift("article")
+    when :style
+      styles = _split_(value)
+
+      if !styles.include?("article")
+        styles.unshift "article"
+      end
+
+      @style = styles
+
+    when :date
+      begin
+        @date = Date.strptime(value, "%Y %B %d")
+      rescue Date::Error
+      end
 
     end
   end
 
-  def validate
+  def _split_(value)
+    value.downcase.split(" / ")
+  end
+
+
+  def fill
+    Files.each do |field|
+    end
+
     if self.vars_str.values.include?(Unset)
       raise ValidationError
     end
   end
 
 
+  def export_internal
+    return self.vars_sym.slice(:head, :capt, :desc, :index, :shard, :date)
+  end
+
+  def export_external
+    return self.vars_sym.slice(:title, :head, :capt, :desc, :index, :shard, :date_displayed)
+  end
+
+
   def to_json
-    return (self
-      .instance_variables
-      .slice(:head, :capt, :desc, :index, :shard, :date)
-      .to_json
-    )
+    return self.export_internal.to_json
+  end
+
+  def to_pretty_json
+    return JSON.pretty_generate(self.export_internal)
   end
 
   def to_yaml
-    return (self
-      .instance_variables
-      .slice(:title, :head, :capt, :desc, :index, :shard, :date_displayed)
-      .to_yaml
-    )
+    return self.export_external.to_yaml
   end
 end
