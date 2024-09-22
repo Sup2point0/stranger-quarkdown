b5607c273b725dfb8c9c765d464a218d9de7b95f
Sup#2.0 <sup2point0@gmail.com>
2024-09-22 16:08:53 +0100

    fix missing object return

--- squarkdown/maps/cleanup.rb
+++ squarkdown/maps/cleanup.rb
@@ -1,23 +1,25 @@
 Cleanup = {
   braces: ->(content) {
-    content = content.gsub!(
+    content = content.gsub(
       /(?<!\{)\{(?!\{)/,
       "&amp;lbrace;"
     )
-    content = content.gsub!(
+    content = content.gsub(
       /(?<!\})\}(?!\})/,
       "&amp;rbrace;"
     )
+    return content
   },
 
   angles: ->(content) {
-    content = content.gsub!(
+    content = content.gsub(
       /</,
       "&amp;lt;"
     )
-    content = content.gsub!(
+    content = content.gsub(
       />/,
       "&amp;gt;"
     )
+    return content
   },
 }
