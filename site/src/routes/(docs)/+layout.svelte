14a4c0b21fd03d7ab6400ae580ce7a994c6ad6f5
Sup#2.0 <sup2point0@gmail.com>
2025-01-28 20:36:38 +0000

    develop routes

---
+++ site/src/routes/(docs)/+layout.svelte
@@ -0,0 +1,64 @@
+<script>
+
+import "#styles/essence.scss";
+import "#styles/prism-tokyo-night.css";
+
+import NavTop from "#parts/nav/nav.bar.svelte";
+import NavLeft from "#parts/nav/nav.pane.svelte";
+import Header from "#parts/page/header.svelte";
+
+import { page } from "$app/state";
+
+
+let { children } = $props();
+
+</script>
+
+
+<div class="ui-root-layout">
+  <NavTop />
+
+  <div class="ui-page-layout">
+    <NavLeft />
+
+    <main>
+      <Header title={page.data.head} />
+
+      <article>
+        {#if children}
+          {@render children()}
+        {:else}
+          <p> Oops, something went wrong! </p>
+        {/if}
+      </article>
+    </main>
+  </div>
+</div>
+
+
+<style lang="scss">
+
+.ui-root-layout {
+  width: 100%;
+  height: 100vh;
+  max-height: 100vh;
+  display: flex;
+  flex-flow: column nowrap;
+}
+
+.ui-page-layout {
+  width: 100%;
+  display: flex;
+  flex-flow: row nowrap;
+  flex-grow: 1;
+  overflow: hidden;
+}
+
+main {
+  width: 100%;
+  padding: 1rem 4rem 3rem 2rem;
+  overflow-y: scroll;
+  background: light-dark(white, black);
+}
+
+</style>
