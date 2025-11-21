12cdc9eb028bdbf251a362b3559ea7a75f85110d
Sup#2.0 <sup2point0@gmail.com>
2025-11-21 14:15:27 +0000

    show last updated date

--- site/src/parts/page/header.svelte
+++ site/src/parts/page/header.svelte
@@ -23,16 +23,31 @@ let { page }: Props = $props();
 <style lang="scss">
 
 header {
+  position: relative;
   padding: 2rem 0;
 }
 
 h1 {
-  width: 80%;
+  width: 72%;
   padding-bottom: 0.25em;
   @include font-ui;
   font-size: 250%;
   font-weight: 300;
   border-bottom: 1px solid $col-charm;
+}
+
+p {
+  position: absolute;
+  bottom: calc(2rem - 5px);
+  right: 0;
+  @include font-ui;
+  color: $col-text-deut;
+  text-align: right;
+
+  span {
+    padding-left: 0.2em;
+    color: $col-down;
+  }
 }
 
 </style>
