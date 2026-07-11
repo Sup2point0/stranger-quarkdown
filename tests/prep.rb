8113e924e9505cdec2a93a86ac324b14e4a49a87
Sup#2.0 <sup2point0@gmail.com>
2026-07-11 17:30:54 +0100

    test - further features were not being tested!

--- tests/prep.rb
+++ tests/prep.rb
@@ -1,37 +1,42 @@
 require "pathname"
+require "minitest/autorun"
 
 require_relative "../squarkdown/scripts/prep-fonts"
 require_relative "../squarkdown/scripts/prep-assets"
 require_relative "../squarkdown/scripts/prep-scss"
 
 
-Current = Pathname(__dir__)
+class Test_Further_Features < Minitest::Test
+
+  Current = Pathname(__dir__)
+
+  RepoConfig = find_repo_config(from: Current)
 
 
-def test_prep_fonts
-  fonts = load_fonts()
-  assert fonts.is_a?(String)
-  assert fonts.include("Testing")
-end
+  def test_prep_fonts
+    prep_fonts(repo_config: RepoConfig)
+  end
 
 
-def test_prep_assets
-  prep_assets()
+  def test_prep_assets
+    prep_assets()
 
-  route = Current / "static"
-  assert route.exist?
-  assert (route / "test.png").exist?
-  assert (route / "test.jpeg").exist?
-  assert (route / "test.svg").exist?
-  assert (route / ".include/test.png").exist?
-end
+    route = Current / "static"
+    assert route.exist?
+    assert (route / "test.png").exist?
+    assert (route / "test.jpeg").exist?
+    assert (route / "test.svg").exist?
+    assert (route / ".include/test.png").exist?
+  end
 
 
-def test_prep_scss
-  scss = load_scss()
-  assert scss.is_a?(Array)
-  assert scss.length == 2
-  assert scss.include?(Current / "resources" / "~test.scss")
-  assert scss.include?(Current / "resources" / "~testing.scss")
-  assert !scss.include?(Current / "resources" / "ignore.scss")
+  def test_prep_scss
+    scss = load_scss()
+    assert scss.is_a?(Array)
+    assert scss.length == 2
+    assert scss.include?(Current / "resources" / "~test.scss")
+    assert scss.include?(Current / "resources" / "~testing.scss")
+    assert !scss.include?(Current / "resources" / "ignore.scss")
+  end
+
 end
