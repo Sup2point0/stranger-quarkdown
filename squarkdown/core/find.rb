a58cdcd1c4d09fc6e4f61e9342bbab3ca0628f2f
Sup#2.0 <sup2point0@gmail.com>
2026-07-12 19:34:35 +0100

    core - refactor `find.rb`

--- squarkdown/core/find.rb
+++ squarkdown/core/find.rb
@@ -1,74 +1,61 @@
+module Squarkdown
+
+require "find"
 require "pathname"
-require "json"
-require "json-schema"
-
-require_relative "../utils/ansi"
-require_relative "../utils/log"
 
 
-
-##
-# Look under `from` for a `.squarkdown/squarkup.json`, and produce a complete repo config, with absent values filled in by defaults.
+## :: keyof RepoConfig.bases -> *Routes -> *RepoConfig -> String
 #
-# This also sets the global `Routes.site` with the value provided by `squarkup.json`.
-def find_repo_config(from: Routes.repo)
-  repo_config = load_default_repo_config()
+# Find the template for `base_type` (`+page.svelte` or `+page.js`).
+def self.find_base_for(base_type, routes:, repo_config:)
 
-  route = from / ".squarkdown/squarkup.json"
-  raise "could not find #{CYAN}squarkup.json" if !route.exist?
+  filename = repo_config.bases.instance_variable_get(base_type)
+  return if filename.nil? or filename.empty?
 
-  content = File.read(route)
+  routes.check_site_resolved()
 
-  data = JSON.parse(content)
+  filepath = routes.site / repo_config.bases.path / filename
+  unless filepath.exist?
+    squark_error("no base for #{BLUE}#{base_type}#{RED} found!", repo_config:)
+  end
 
-  JSON::Validator.validate!(SquarkupSchema, data)
-  
-  repo_config.merge!(data)
-  Routes.site = Routes.repo / repo_config["paths / site"]
+  log success: "found base for #{WHITE}#{base_type}#{CYAN}: #{BLUE}#{filepath}"
 
-  return repo_config
-end
-
-
-##
-# Find template for `base_type` (`+page.svelte` or `+page.js`)
-def find_file_base(base_type, from: Routes.site, repo_config:)
-  filepath = repo_config[base_type]
-  return nil if filepath.nil? or filepath.empty?
-
-  route = from / repo_config["bases / path"] / filepath
-  raise "no base for #{BLUE}#{base_type}#{RED} found!" unless route.exist?
-
-  out = File.read(route)
-  log success: "found base for #{BLUE}#{base_type}#{CYAN}"
+  out = File.read(filepath)
+  if out.nil?
+    squark_error("#{WHITE}#{base_type} #{RED}appears to be empty!", repo_config:)
+  end
 
   return out
 end
 
 
-##
-# Recursively search for `.md` files to squarkup
-def find_files(from: nil, repo_config:)
-  from = Routes.repo unless !from.nil?
+## :: *Routes -> *RepoConfig -> [Pathname]
+#
+# Recursively search for `.md` files to squarkup.
+def self.find_files_to_squarkup(routes:, repo_config:)
 
-  sources = repo_config["paths / sources"]
+  from = routes.repo
+  sources = repo_config.paths.sources
 
-  if !sources.nil? && !sources.empty?
-    paths = sources.map do |path|
-      if path == "." || path == "./"
-        from.glob "*.md", File::FNM_DOTMATCH
-      else
-        (from / path).glob "**/*.md", File::FNM_DOTMATCH
+  paths = (
+    unless sources.nil? or sources.empty?
+      sources.flat_map do |path|
+        if path == "." or path == "./"
+          from.glob("*.md", File::FNM_DOTMATCH)
+        else
+          # FIXME ignore .git, etc.
+          (from / path).glob("**/*.md", File::FNM_DOTMATCH)
+        end
       end
+    else
+      from.glob("**/*.md", File::FNM_DOTMATCH)
     end
-    paths = paths.flatten
-  else
-    paths = from.glob "**/*.md", File::FNM_DOTMATCH
-  end
+  )
 
-  exclude = repo_config["paths / exclude"]
+  exclude = repo_config.paths.exclude
 
-  if exclude
+  unless exclude.nil? or exclude.empty?
     paths.filter! do |path|
       (exclude.map { |pattern|
         path.realpath.to_s.match(pattern)
@@ -77,4 +64,7 @@ def find_files(from: nil, repo_config:)
   end
 
   return paths
+end
+
+
 end
