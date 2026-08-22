0d6a90810827dd34fef0b63723d60f3437a43859
Sup#2.0 <sup2point0@gmail.com>
2026-08-22 17:36:14 +0100

    test - migrate space -> tab indentation

--- tests/test-find.rb
+++ tests/test-find.rb
@@ -5,33 +5,33 @@ require_relative "../squarkdown/core/find"
 
 class Test_Find_ < Minitest::Test
 
-  def test_find_repo_config
-    config = find_repo_config(from: Tests)
-    
-    assert !config.nil?
-    assert config.is_a?(Hash)
-    assert config["repo"] == "Squarkdown Tests"
-  end
+	def test_find_repo_config
+		config = find_repo_config(from: Tests)
+		
+		assert !config.nil?
+		assert config.is_a?(Hash)
+		assert config["repo"] == "Squarkdown Tests"
+	end
 
-  
-  def test_find_files
-    files = find_files(from: Tests.parent, repo_config: RepoConfig)
+	
+	def test_find_files
+		files = find_files(from: Tests.parent, repo_config: RepoConfig)
 
-    assert_operator files.length, :>, 0
-    assert_includes files, (Tests / "content/test.md")
-  end
-
-
-  def test_find_no_files
-    files = find_files(from: Tests / "ignore", repo_config: RepoConfig)
-    assert_empty files
-  end
+		assert_operator files.length, :>, 0
+		assert_includes files, (Tests / "content/test.md")
+	end
 
 
-  def test_ignore_files
-    files = find_files(from: Tests, repo_config: RepoConfig)
+	def test_find_no_files
+		files = find_files(from: Tests / "ignore", repo_config: RepoConfig)
+		assert_empty files
+	end
 
-    assert files.length == 0
-  end
+
+	def test_ignore_files
+		files = find_files(from: Tests, repo_config: RepoConfig)
+
+		assert files.length == 0
+	end
 
 end
--- tests/test-prep.rb
+++ tests/test-prep.rb
@@ -5,75 +5,75 @@ require_relative "../squarkdown/scripts/prep-scss"
 
 class Test_Further_Features_  < Minitest::Test
 
-  def test_prep_fonts
-    path = TestSite / "src/app.html"
-    before = File.read(path)
+	def test_prep_fonts
+		path = TestSite / "src/app.html"
+		before = File.read(path)
 
-    prep_fonts(repo_config: RepoConfig)
-    after = File.read(path)
+		prep_fonts(repo_config: RepoConfig)
+		after = File.read(path)
 
-    assert before != after
-    assert_includes after, "display=swap"
-    assert_includes after, "Test+Sans"
-    assert_includes after, "</head>"
+		assert before != after
+		assert_includes after, "display=swap"
+		assert_includes after, "Test+Sans"
+		assert_includes after, "</head>"
 
-    File.write(path, before)
-  end
-
-
-  def test_prep_assets
-    prep_assets(repo_config: RepoConfig)
-
-    path = TestSite / "static"
-    assert_path_exists path
-    
-    assert_path_exists (path / "squark-cover.png")
-    assert_path_exists (path / "squark-icon.png")
-
-    assert !(path / "site").exist?
-    assert_path_exists (path / "arrow.svg")
-    assert_path_exists (path / "copy.svg")
-    assert_path_exists (path / "tick.svg")
-  end
+		File.write(path, before)
+	end
 
 
-  def test_prep_scss
-    prep_scss(repo_config: RepoConfig)
+	def test_prep_assets
+		prep_assets(repo_config: RepoConfig)
 
-    path = TestSite / "scss-config.js"
-    assert_path_exists path
+		path = TestSite / "static"
+		assert_path_exists path
+		
+		assert_path_exists (path / "squark-cover.png")
+		assert_path_exists (path / "squark-icon.png")
 
-    after = File.read(path)
-    assert !after.nil?, "failed to read #{path}"
+		assert !(path / "site").exist?
+		assert_path_exists (path / "arrow.svg")
+		assert_path_exists (path / "copy.svg")
+		assert_path_exists (path / "tick.svg")
+	end
 
-    assert_includes after, "scssConfig"
-    assert_includes after, "export default scssConfig"
-    assert_includes after, "includePaths"
-    assert_includes after, RepoConfig["styles / path"]
 
-    assert_includes after, "~article"
-    assert_includes after, "~mixins"
-    assert_includes after, "~testing"
-    assert !after.include?("ignore")
-    
-    assert_operator (after.index "~article"), :<, (after.index "~mixins")
-    assert_operator (after.index "~mixins"), :<, (after.index "~testing")
+	def test_prep_scss
+		prep_scss(repo_config: RepoConfig)
 
-    assert_includes(after,
+		path = TestSite / "scss-config.js"
+		assert_path_exists path
+
+		after = File.read(path)
+		assert !after.nil?, "failed to read #{path}"
+
+		assert_includes after, "scssConfig"
+		assert_includes after, "export default scssConfig"
+		assert_includes after, "includePaths"
+		assert_includes after, RepoConfig["styles / path"]
+
+		assert_includes after, "~article"
+		assert_includes after, "~mixins"
+		assert_includes after, "~testing"
+		assert !after.include?("ignore")
+		
+		assert_operator (after.index "~article"), :<, (after.index "~mixins")
+		assert_operator (after.index "~mixins"), :<, (after.index "~testing")
+
+		assert_includes(after,
 
 "
 const scssConfig = {
-  includePaths: [\"src/styles/\"],
-  prependData: `
-    @use '~article' as *;
-    @use '~mixins' as *;
-    @use '~testing' as *;
-  `
+	includePaths: [\"src/styles/\"],
+	prependData: `
+		@use '~article' as *;
+		@use '~mixins' as *;
+		@use '~testing' as *;
+	`
 };
 export default scssConfig;
 "
-    )
-  
-  end
+		)
+	
+	end
 
 end
