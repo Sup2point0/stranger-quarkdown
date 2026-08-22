ca7611c167a2ce73837cabdb5e2f18a9de98df3c
Sup#2.0 <sup2point0@gmail.com>
2026-08-22 19:40:47 +0100

    test - remove `test-render` tests

--- tests/test-render.rb
+++ tests/test-render.rb
@@ -1,10 +1,8 @@
-require "minitest/autorun"
-
-require_relative "utils"
-
-require_relative "../squarkdown/types/file-data"
 require_relative "../squarkdown/core/render"
 require_relative "../squarkdown/core/find"
+require_relative "../squarkdown/types/file-data"
+
+require_relative "shared"
 
 
 def _get_data_(repo_config:)
@@ -18,69 +16,17 @@ end
 
 class SquarkupRender < Minitest::Test
 
-	RepoConfig = load_default_repo_config().merge({
-		"repo" => "Squarkdown Tests",
-		"paths / site" => "stranger-quarkdown/tests/",
-		"styles / path" => "resources/",
-		"styles / page-styles" => "resources/",
-		"styles / base-style" => "basic",
-	})
-
-	Data = _get_data_(repo_config: RepoConfig)
+	Data = _get_data_(repo_config: TestConfig)
 
 	
 	def test_head
-		content = """# Testing
-		"""
+		content = """# Testing"""
 
-		out = inject_head(content, data: Data, repo_config: RepoConfig)
+		out = Squarkdown.inject_head!(content, file_data: Data, repo_config: TestConfig)
 
 		assert out.include?("<title> Squarkdown is epic · Squarkdown Tests </title>"), (got out)
 	end
 
-
-	def test_style
-		content = """# Testing
-		"""
-
-		out = inject_style(content, data: Data, repo_config: RepoConfig)
-
-		assert out.include?("resources/basic")
-		assert out.include?("resources/testing")
-	end
-
-
-	def test_repl
-		content = """# Testing
-
-<!-- #SQUARK leave? -->
-unprocessed content
-<!-- #SQUARK leave. -->
-
-<!-- #SQUARK only?
-processed content
-			#SQUARK only. -->
-		"""
-
-		out = inject_repl(content)
-
-		assert !out.include?("unprocessed content")
-		assert out.include?("processed content")
-		assert !out.include?("#SQUARK only")
-	end
-
-	def test_clean
-		content = """# Testing
-
-{surround}
-{{ignore}}
-"""
-
-		out = cleanup(content, data: Data)
-
-		assert out.include?("&amp;lbrace;surround&amp;rbrace;")
-		assert out.include?("{{ignore}}")
-		assert !out.include?("&amp;lbrace;ignore&amp;rbrace;")
-	end
+	# TODO FIXME
 
 end
