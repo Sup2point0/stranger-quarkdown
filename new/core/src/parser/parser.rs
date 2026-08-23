8d8e1dd7e12cd71138c9bdef2cbb38df61090f14
Sup#2.0 <sup2point0@gmail.com>
2026-08-23 11:00:52 +0100

    【core】retire `BufferedParser`, switch `Chars` to `Vec<char>`

--- new/core/src/parser/parser.rs
+++ new/core/src/parser/parser.rs
@@ -1,6 +1,6 @@
 use std::{
 	fs::File,
-	io::BufReader, io::BufRead,
+	io::{ BufReader, BufRead, Read },
 	iter,
 	str::Chars,
 };
@@ -10,39 +10,49 @@ use crate::{ log, SquarkupConfig, FileData };
 
 
 /// A parser for the 'squark charm' header of a file.
-pub struct CharmParser<'l>
+pub struct CharmParser<'l, Source: Read = File>
 {
 	config: &'l SquarkupConfig,
-	
-	/// The current character the parser is pointing to.
-	current: Option<char>,
 	
 	/// Have we encountered a `<!-- #SQUARK live!` yet?
 	///
 	/// If so, this means the user intends for the file to be squarked up, and error checking should be stricter to catch mistakes on their end.
 	is_live: bool,
+
+	// == INTERNALS == //
 	
 	/// The backing buffer that reads from the target file.
-	_reader: BufReader<File>,
+	_reader: BufReader<Source>,
+
+	/// The current index in the current chunk the parser is pointing to.
+	_index: usize,
+
+	/* NOTE:
+		Storing a `Chars` iterator over `_chunk_buffer` caused lifetime issues =(
+		Having another `Vec<char>` is a little duplication, but it does make it much nicer to work with
+	*/
+
+	/// Individual characters of the currently in-memory chunk to process.
+	_chunk: Vec<char>,
 	
 	/// The currently in-memory chunk to process.
-	_chunk: String,
-	
-	/// An iterator over the characters of the current chunk.
-	_chars: Chars<'l>,
+	/// 
+	/// This backs `._chunk`. Reuse this between line reads to avoid excessive allocations!
+	_chunk_buffer: String,
 }
 
-impl<'l> CharmParser<'l>
+/// The public parser interface.
+impl<'l, Source: Read> CharmParser<'l, Source>
 {
-	pub fn init(file: File, config: &'l SquarkupConfig) -> Self
+	pub fn init(file: Source, config: &'l SquarkupConfig) -> Self
 	{
 		let mut out = Self {
 			config,
-			current: None,
-			is_live: false,
 			_reader: BufReader::new(file),
-			_chunk: String::new(),
-			_chars: "".chars(),
+			_index: 0,
+			_chunk: vec![],
+			_chunk_buffer: String::new(),
+			is_live: false,
 		};
 		
 		out.next_line();
@@ -62,14 +72,18 @@ impl<'l> CharmParser<'l>
 			},
 		}
 	}
-	
+}
+
+/// *Specialised* parser internals, specialised to Squarkdown-Flavoured Markdown.
+impl<'l, Source: Read> CharmParser<'l, Source>
+{
 	fn _parse(&mut self) -> Result<FileData, ParseError>
 	{
 		self.eat_spaces()?;
 		
 		let mut heading = None;
 		
-		if self.current == Some('#') {
+		if self.current() == Some('#') {
 			heading = Some(self.parse_heading()?);
 		}
 		
@@ -83,7 +97,7 @@ impl<'l> CharmParser<'l>
 	
 	fn parse_heading(&mut self) -> ParseResult<String>
 	{
-		self.eat("#".chars());
+		self.eat("#");
 		unimplemented!()
 	}
 	
@@ -91,16 +105,37 @@ impl<'l> CharmParser<'l>
 	{
 		unimplemented!()
 	}
+
+	fn err(&self) -> ParseError
+	{
+		if self.is_live {
+			ParseError::FatalEnd
+		} else {
+			ParseError::NoMatch
+		}
+	}
 }
 
-impl<'l> BufferedParser for CharmParser<'l>
+/// *Generic* parser internals, not specific to Squarkdown-Flavoured Markdown.
+impl<'l, Source: Read> CharmParser<'l, Source>
 {
+	/// The current character in the source the parser is pointing to, or `None` if it is out of bounds.
+	fn current(&self) -> Option<char>
+	{
+		self._chunk.get(self._index).map(|c| *c)
+	}
+
 	/// Read the next line of the source text into memory.
 	fn next_line(&mut self) -> ParseResult
 	{
-		self._reader.read_line(&mut self._chunk)?;
-		self._chars = self._chunk.chars();
-		self.current = self._chars.next();
+		match self._reader.read_line(&mut self._chunk_buffer) {
+			Ok(0) => return Err(ParseError::EndOfFile),
+			Err(_) => return Err(self.err()),
+			Ok(_) => (),
+		}
+
+		self._chunk = self._chunk_buffer.chars().collect();
+		self._index = 0;
 
 		Ok(())
 	}
@@ -108,22 +143,19 @@ impl<'l> BufferedParser for CharmParser<'l>
 	/// Proceed to the next character in the source text.
 	fn advance(&mut self) -> ParseResult
 	{
-		self.current = self._chars.next();
-		
-		if self.current == None {
-			self.next_line()?;
+		if self.current() == None {
+			self.next_line()
 		}
-		
-		Err(if self.is_live {
-			ParseError::FatalEnd
-		} else {
-			ParseError::NoMatch
-		})?
+		else {
+			self._index += 1;
+			Ok(())
+		}
 	}
 	
-	fn eat(&mut self, mut chars: impl Iterator<Item = char>) -> ParseResult
+	fn eat(&mut self, chars: &str) -> ParseResult
 	{
 		// TODO
+		let mut chars = chars.chars();
 
 		loop {
 			let Some(required) = chars.next() else {
@@ -136,10 +168,28 @@ impl<'l> BufferedParser for CharmParser<'l>
 	
 	fn eat_spaces(&mut self) -> ParseResult
 	{
-		while self.current == Some(' ') {
+		while self.c == Some(' ') {
 			self.advance()?;
 		}
 
 		Ok(())
+	}
+}
+
+
+#[cfg(test)]
+mod test
+{
+	use std::io::Cursor;
+
+	use crate::parser::CharmParser;
+	use crate::utils::*;
+
+	#[test] fn test_eat()
+	{
+		let source = Cursor::new("test");
+		let mut parser = CharmParser::init(source, &TEST_CONFIG);
+		let r = parser.eat("test");
+		assert_eq!(r, Ok(()));
 	}
 }
