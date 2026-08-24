3ca5c838fc42ffb4dc488e51f2d92579dc5e1b9f
Sup#2.0 <sup2point0@gmail.com>
2026-08-24 11:32:57 +0100

    【core】setup main executable skeleton

--- new/core/src/main.rs
+++ new/core/src/main.rs
@@ -1,4 +1,40 @@
-fn main()
+use squarkdown::{
+	resolver,
+	SiteData, CharmParser, Renderer,
+	utils::log,
+};
+
+use std::fs::File;
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>>
 {
-	unimplemented!()
+	let project_root = resolver::resolve_project_root();
+	let config = resolver::load_config(project_root);
+	
+	let files = resolver::find_files(&config);
+	
+	if files.is_empty() {
+		log::bad!("No files found to squarkup, exiting!");
+		return Ok(());
+	}
+	
+	let site_data = SiteData::new();
+	
+	for filepath in files {
+		let mut parser = CharmParser::init(File::open(filepath)?, &config)?;
+		
+		if let Some(file_data) = parser.parse() {
+			log::ok!("found active file: {BLUE}{filepath}");
+			site_data.set(filepath, file_data);
+		}
+	}
+	
+	for file in &site_data.files {
+		let renderer = Renderer::init(File::open(file.source), &config);
+		
+		renderer.render()?;
+	}
+	
+	Ok(())
 }
