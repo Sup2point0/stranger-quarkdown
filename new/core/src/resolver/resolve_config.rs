851b801f550ed22ee51d1bdb2caf3e68820e2588
Sup#2.0 <sup2point0@gmail.com>
2026-08-24 19:35:59 +0100

    【core】start implementing `load_config()`

--- new/core/src/resolver/resolve_config.rs
+++ new/core/src/resolver/resolve_config.rs
@@ -1,53 +1,63 @@
-use toml::Table;
-
 use super::{ ResolutionResult, ResolutionError };
-use crate::SquarkupConfig;
+use crate::{
+	SquarkupConfig,
+	utils::macros::*,
+};
 
-use std::fs::File;
+use std::fs::{ File };
+use std::io::{ Read };
 use std::path::{ PathBuf, Path };
 
 
-/// Find, load and validate the user's squarkup configuration, either in `squarkup.toml` or `squarkup.json`.
+/// Find, read, load and validate the user's squarkup configuration, either in `squarkup.toml` or `squarkup.json`.
 pub fn resolve_config(root: PathBuf) -> ResolutionResult<SquarkupConfig>
 {
 	let filepath = find_config(&root)?;
+	// let defaults = load_config_defaults()?;
+	let config = load_config(root, filepath);
 	unimplemented!()
 }
 
 /// Find the location of the user's squarkup configuration, in `(.squarkdown/)?squarkup.(toml|json)`.
 fn find_config(root: &Path) -> ResolutionResult<PathBuf>
 {
-	let folder = root.join(".squarkdown");
-
-	if folder.exists() && folder.is_dir() {
-		let file = folder.join("squarkup.toml");
-		if file.exists() && file.is_file() {
-			return Ok(file);
-		}
-		
-		let file = folder.join("squarkup.json");
-		if file.exists() && file.is_file() {
-			return Ok(file);
-		}
-	}
-	
-	let file = root.join("squarkup.toml");
-	if file.exists() && file.is_file() {
-		return Ok(file);
-	}
-	
-	let file = root.join("squarkup.json");
-	if file.exists() && file.is_file() {
-		return Ok(file);
-	}
-
-	Err(ResolutionError::NoConfig)
+	let Some(out) = find_config_from(&dir!(root / ".squarkdown/")) else {
+		let Some(out) = find_config_from(root) else {
+			return Err(ResolutionError::NoConfig);
+		};
+		return Ok(out);
+	};
+	return Ok(out);
 }
 
-fn load_config(filepath: PathBuf) -> ResolutionResult<SquarkupConfig>
+fn find_config_from(folder: &Path) -> Option<PathBuf>
 {
-	let file = File::open(filepath)?;
-	let content = file.
+	let file = folder.join("squarkup.toml");
+	if file.exists() && file.is_file() {
+		return Some(file);
+	}
+	
+	let file = folder.join("squarkup.json");
+	if file.exists() && file.is_file() {
+		return Some(file);
+	}
 
-	unimplemented!()
+	None
+}
+
+/// Read and parse the user's squarkup configuration.
+fn load_config(root: PathBuf, filepath: PathBuf) -> ResolutionResult<SquarkupConfig>
+{
+	let Ok(mut file) = File::open(&filepath) else {
+		return Err(ResolutionError::ReadError { target: filepath.display().to_string() });
+	};
+
+	let mut content = String::new();
+	let Ok(_) = file.read_to_string(&mut content) else {
+		return Err(ResolutionError::ReadError { target: filepath.display().to_string() });
+	};
+
+	let data = content.parse::<toml::Table>().expect("invalid squarkup config");
+
+	SquarkupConfig::try_from_toml(data)
 }
