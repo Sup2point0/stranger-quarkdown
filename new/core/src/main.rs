8a88b2fb7163d3a36f7691de53a23b257bb05648
Sup#2.0 <sup2point0@gmail.com>
2026-08-24 17:13:33 +0100

    【core】remove unnecessary `.clone()`s for `PathBuf`s in `main`

--- new/core/src/main.rs
+++ new/core/src/main.rs
@@ -43,9 +43,9 @@ fn squarkup() -> Result<bool, Box<dyn std::error::Error>>
 	let project_root = resolver::resolve_project_root()?;
 	log::ok!("found your project: {BLUE}{}", project_root.display());
 
-	let config = resolver::load_config(project_root);
+	let config = resolver::resolve_config(project_root)?;
 	
-	let files = resolver::find_files(&config);
+	let files = resolver::find_files(&config)?;
 	
 	if files.is_empty() {
 		log::bad!("No files found to squarkup, exiting!");
@@ -57,7 +57,7 @@ fn squarkup() -> Result<bool, Box<dyn std::error::Error>>
 	let mut site_data = SiteData::new();
 	
 	for filepath in files {
-		let file = File::open(filepath.clone())?;
+		let file = File::open(&filepath)?;
 		let mut parser = CharmParser::init(file, Some(filepath.clone()))?;
 		
 		if let Some(page_data) = parser.parse(&config)? {
@@ -68,7 +68,7 @@ fn squarkup() -> Result<bool, Box<dyn std::error::Error>>
 	
 	for page in site_data.pages.values() {
 		let dest = dir!(config.paths.root / &page.destination);
-		let source = File::open(page.filepath.clone())?;
+		let source = File::open(&page.filepath)?;
 		let target = File::create(dest)?;
 
 		let mut renderer = Renderer::init(source, target);
