bacfd9978aa77ebb5248c437868f79e716f0b7c9
Sup#2.0 <sup2point0@gmail.com>
2026-08-24 14:21:14 +0100

    【core】cleanup errors with `thiserror`

--- new/core/src/types/cleanse.rs
+++ new/core/src/types/cleanse.rs
@@ -1,11 +1,14 @@
-use super::FileError;
 use crate::utils::macros::*;
+
+use enum_stringify::EnumStringify;
 
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[derive(EnumStringify)] #[enum_stringify(case = "flat")]
 pub enum CleanseOperation
 {
 	/// Replace non-tag `<>` with `&lt;`, `&gt;` for HTML safety.
+	#[enum_stringify(case = "flat")]
 	ANGLES,
 
 	/// Replace `{}` with `&lbrace;`, `&rbrace;` for HTML safety.
@@ -17,24 +20,4 @@ pub enum CleanseOperation
 	/// Remove `\n<br>\n` large line breaks.
 	#[allow(non_camel_case_types)]
 	LINE_BREAKS,
-}
-
-impl TryFrom<String> for CleanseOperation
-{
-	type Error = FileError;
-
-	fn try_from(mut value: String) -> Result<Self, Self::Error>
-	{
-		value.make_ascii_lowercase();
-
-		match value.as_str()
-		{
-			"angles" => Ok(Self::ANGLES),
-			"braces" => Ok(Self::BRACES),
-			"comments" => Ok(Self::COMMENTS),
-			"line-breaks" | "line breaks" => Ok(Self::LINE_BREAKS),
-			
-			_ => Err(FileError::InvalidValue { field: str!("cleanse"), value })
-		}
-	}
 }
--- new/core/src/types/file_error.rs
+++ new/core/src/types/file_error.rs
@@ -1,26 +1,16 @@
-#[derive(Clone, Debug, PartialEq, Eq)]
+use thiserror::Error;
+
+#[derive(Clone, Debug, Error, PartialEq, Eq)]
 pub enum FileError
 {
+	#[error("missing field: {field}")]
 	MissingField {
 		field: String,
 	},
 
+	#[error("invalid value: {value}, for field: {field}")]
 	InvalidValue {
 		field: String,
 		value: String,
 	},
-}
-
-impl std::error::Error for FileError {}
-
-impl std::fmt::Display for FileError
-{
-	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
-	{
-		match self
-		{
-			Self::MissingField { field } => write!(f, "missing field: {field}"),
-			Self::InvalidValue { field, value } => write!(f, "invalid value: {value}, for field: {field}"),
-		}
-	}
 }
