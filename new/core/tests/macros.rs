a1833121f8119b139ccc2185a6af764a5f625733
Sup#2.0 <sup2point0@gmail.com>
2026-08-24 10:31:05 +0100

    【core】copy macros to tests

---
+++ new/core/tests/macros.rs
@@ -0,0 +1,34 @@
+macro_rules! str {
+	($t:expr) => {
+		String::from($t)
+	};
+}
+pub(crate) use str;
+
+
+macro_rules! a_str {
+	($t:expr) => {
+		|| String::from($t)
+	};
+}
+pub(crate) use a_str;
+
+
+macro_rules! strings
+{
+	() => {
+		tiny_vec!([String; 4])
+	};
+	($value:expr $(, $rest:expr)* $(,)?) => {
+		tiny_vec!([String; 4] => str!($value) $(, str!($rest))*)
+	};
+}
+pub(crate) use strings;
+
+
+macro_rules! dir {
+	($base:ident $(/ $part:expr)*) => {
+		$base$(.join($part))*
+	};
+}
+pub(crate) use dir;
--- new/core/tests/mod.rs
+++ new/core/tests/mod.rs
@@ -1,3 +1,6 @@
+/* NOTE: This file is directly copied from `src/utils/macros.rs`, since we want these utils to be available in both `src/` and `test/`, but not publically exposed */
+mod macros;
+
 mod parser {
 	mod test_fields;
 	mod test_heading;
