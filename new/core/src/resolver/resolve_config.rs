48f4b04c212789bf1feffad9a32f0366d6e66b06
Sup#2.0 <sup2point0@gmail.com>
2026-08-24 21:30:26 +0100

    【core】separate loading logic for TOML and JSON

--- new/core/src/resolver/resolve_config.rs
+++ new/core/src/resolver/resolve_config.rs
@@ -1,26 +1,39 @@
-use super::{ ResolutionResult, ResolutionError };
 use crate::{
 	SquarkupConfig, SquarkResult, SquarkError,
 	utils::colours::*,
 	utils::macros::*,
 };
 
+use std::error::Error;
 use std::fs::{ File };
 use std::io::{ Read };
 use std::path::{ PathBuf, Path };
 
 
+enum Extension { TOML, JSON }
+
+
 /// Find, read, load and validate the user's squarkup configuration, either in `squarkup.toml` or `squarkup.json`.
 pub fn resolve_config(root: PathBuf) -> SquarkResult<SquarkupConfig>
 {
-	let filepath = find_config(&root)?;
-	// let defaults = load_config_defaults()?;
-	let config = load_config(root, filepath);
-	unimplemented!()
+	let (filepath, ext) = find_config(&root)?;
+
+	let config = SquarkupConfig::init_defaults(root.clone());
+
+	match ext {
+		Extension::TOML => {
+			let data = read_toml_config(filepath)?;
+			config.set_from_toml(data, root)
+		},
+		Extension::JSON => {
+			unimplemented!()
+		},
+		_ => unreachable!("`find_config()` should fail if the extension is not .toml or .json"),
+	}
 }
 
 /// Find the location of the user's squarkup configuration, in `(.squarkdown/)?squarkup.(toml|json)`.
-fn find_config(root: &Path) -> SquarkResult<PathBuf>
+fn find_config(root: &Path) -> SquarkResult<(PathBuf, Extension)>
 {
 	if let Some(out) = find_config_from(&dir!(root / ".squarkdown/")) {
 		return Ok(out);
@@ -31,7 +44,8 @@ fn find_config(root: &Path) -> SquarkResult<PathBuf>
 	};
 
 	Err(SquarkError::Unrecoverable {
-		msg: format!("could not find your {WHITE}squarkup.toml{RED} or {WHITE}squarkup.json{RED}"),
+		msg: str!("could not find your squarkup configuration file"),
+		hint: format!("make sure you have either a {WHITE}.squarkdown/{RED} folder, or a {WHITE}squarkup.toml{RED} or {WHITE}squarkup.json{RED} file, in the root of your project"),
 		debug: vec![format!(
 			"looked in {WHITE}{}{GREY} and {WHITE}{}",
 			root.display(),
@@ -40,34 +54,36 @@ fn find_config(root: &Path) -> SquarkResult<PathBuf>
 	})
 }
 
-fn find_config_from(folder: &Path) -> Option<PathBuf>
+fn find_config_from(folder: &Path) -> Option<(PathBuf, Extension)>
 {
 	let file = folder.join("squarkup.toml");
 	if file.exists() && file.is_file() {
-		return Some(file);
+		return Some((file, Extension::TOML));
 	}
 	
 	let file = folder.join("squarkup.json");
 	if file.exists() && file.is_file() {
-		return Some(file);
+		return Some((file, Extension::JSON));
 	}
+
+	// TODO: funny messages on close matches
+	// if glob::glob(folder.join(""))
 
 	None
 }
 
-/// Read and parse the user's squarkup configuration.
-fn load_config(root: PathBuf, filepath: PathBuf) -> SquarkResult<SquarkupConfig>
+/// Read the user's squarkup configuration from `squarkup.toml`.
+fn read_toml_config(filepath: PathBuf) -> SquarkResult<toml::Table>
 {
-	(|| -> Result<(), _> {
+	(|| -> Result<toml::Table, Box<dyn Error>>
+	{
 		let mut file = File::open(&filepath)?;
 
-		let mut content = String::new();
+		let mut content = str!();
 		file.read_to_string(&mut content)?;
 
 		let data = content.parse::<toml::Table>()?;
-
-		Ok(())
-	})().map_err(err!())?;
-
-	SquarkupConfig::try_from_toml(data, root)
+		Ok(data)
+	})
+	().map_err(|e| SquarkError::External(e))
 }
