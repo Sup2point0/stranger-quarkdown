dda2441e0bcb2d5e5ea47d5c47d3a4bd038805dc
Sup#2.0 <sup2point0@gmail.com>
2026-08-25 15:16:39 +0100

    【core】use default `SquarkupConfig` for unit tests

--- new/core/src/utils/testing.rs
+++ new/core/src/utils/testing.rs
@@ -1,6 +1,5 @@
 use lazy_static::lazy_static;
 
-use super::macros::*;
 use crate::{
 	config::*,
 };
@@ -13,23 +12,6 @@ lazy_static!
 {
 	pub static ref TESTS: PathBuf = std::env::current_dir().unwrap().join("tests/test-project");
 	
-	/// A testing squarkup config used for unit tests.
-	pub static ref TEST_CONFIG: SquarkupConfig = SquarkupConfig
-	{
-		paths: PathsConfig {
-			root: TESTS.clone(),
-			site: dir!(TESTS / "test-site"),
-			dest: dir!(TESTS / "test-site/src/routes/test-gen"),
-			sources: vec![TESTS.clone()],
-			include: vec![str!(r#"\.txt$"#)],
-			include_patterns: vec![],
-			exclude: vec![str!("ignored"), str!(r#"/_*\.md"#)],
-			exclude_patterns: vec![],
-			default_exclude: true,
-		},
-		errors: ErrorConfig {
-			on_error: ErrorAction::KILL,
-			on_file_exists: FileAction::OVERWRITE,
-		}
-	};
+	/// A barebones testing squarkup config used for unit tests.
+	pub static ref TEST_CONFIG: SquarkupConfig = SquarkupConfig::init_defaults(&TESTS, &TESTS);
 }
