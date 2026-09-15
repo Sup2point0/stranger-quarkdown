bad93b24b2aab1b3ed791b3bcd8193bb303bbe64
Sup#2.0 <sup2point0@gmail.com>
2026-09-15 10:34:27 +0100

    【render】add link rewriting integration tests

--- new/core/tests/links/basic/main.md
+++ new/core/tests/links/basic/main.md
@@ -4,5 +4,3 @@
 -->
 
 This link to [side](./side.md) should be rewritten to `./side`.
-
-This link to [side](./side.md#section) should be rewritten to `./side#section`.
--- new/core/tests/links/mod.rs
+++ new/core/tests/links/mod.rs
@@ -1,8 +1,41 @@
-#[test] fn links()
+use std::assert_matches;
+
+
+/// Squarkdown rewrites links.
+#[test] fn basic()
 {
 	let bin = env!("CARGO_BIN_EXE_squarkdown");
 
-	std::process::Command::new(bin)
-		.current_dir(std::env::current_dir().unwrap().join("tests/links"))
-		.spawn().unwrap();
+	let r = std::process::Command::new(bin)
+		.current_dir(std::env::current_dir().unwrap().join("tests/links/basic"))
+		.status();
+
+	assert_matches!( r, Ok(..) );
+	assert!( r.unwrap().success() );
+}
+
+/// Squarkdown resolves links with anchors (`path/to/page.md#anchor`) while keeping the anchor.
+#[test] fn anchors()
+{
+	let bin = env!("CARGO_BIN_EXE_squarkdown");
+
+	let r = std::process::Command::new(bin)
+		.current_dir(std::env::current_dir().unwrap().join("tests/links/anchors"))
+		.status();
+
+	assert_matches!( r, Ok(..) );
+	assert!( r.unwrap().success() );
+}
+
+/// With `linked-file-inactive: error`, Squarkdown cesrash when encountering links to inactive pages.
+#[test] fn inactive()
+{
+	let bin = env!("CARGO_BIN_EXE_squarkdown");
+
+	let r = std::process::Command::new(bin)
+		.current_dir(std::env::current_dir().unwrap().join("tests/links/inactive-crash"))
+		.status();
+
+	assert_matches!( r, Ok(..) );
+	assert!( !r.unwrap().success() );
 }
