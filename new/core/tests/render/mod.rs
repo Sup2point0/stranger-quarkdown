b534fc31d5f644739b051d5f64bea6e5b58d1272
Sup#2.0 <sup2point0@gmail.com>
2026-09-15 17:20:21 +0100

    【render】repair tests

--- new/core/tests/render/mod.rs
+++ new/core/tests/render/mod.rs
@@ -4,15 +4,15 @@ use assertables::*;
 
 
 /// Squarkdown exports files to the right place.
-#[test] fn location()
+#[test] fn dest()
 {
-	assert!( squarkup_from("render/location").success() );
+	assert!( squarkup_from("render/dest").success() );
 
-	let f1 = read_file("render/location/1/+page.svx");
-	let f2 = read_file("render/location/nest/2/+page.svx");
-	let f3 = read_file("render/location/nest/nest/3/+page.svx");
-	let f4 = read_file("render/location/nest/nest/nest/4/+page.svx");
-	let f5 = read_file("render/location/nest/nest/nest/nest/5/+page.svx");
+	let f1 = read_file("render/dest/1/+page.svx");
+	let f2 = read_file("render/dest/nest/2/+page.svx");
+	let f3 = read_file("render/dest/nest/nest/3/+page.svx");
+	let f4 = read_file("render/dest/nest/nest/nest/4/+page.svx");
+	let f5 = read_file("render/dest/nest/nest/nest/nest/5/+page.svx");
 	assert_contains!( f1, "# 1" );
 	assert_contains!( f2, "# 2" );
 	assert_contains!( f3, "# 3" );
