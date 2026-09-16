4fedb2296ff63cb523668fc0ab3919b575768bcc
Sup#2.0 <sup2point0@gmail.com>
2026-09-16 21:10:48 +0100

    【core】add many conflict tests

--- new/core/tests/errors/mod.rs
+++ new/core/tests/errors/mod.rs
@@ -3,9 +3,21 @@ use crate::*;
 use assertables::*;
 
 
-/// Squarkdown errors on page conflicts with `config.errors.strict = true`.
+/// Squarkdown errors on 2-page conflicts with `config.errors.strict = true`.
 #[test] fn conflicts()
 {
 	let r = capture_squarkup_from("errors/conflicts");
 	assert_contains!( r, "conflict" );
+	assert_contains!( r, "left.md" );
+	assert_contains!( r, "right.md" );
+}
+
+/// Squarkdown errors on 3-page conflicts with `config.errors.strict = true`.
+#[test] fn many_conflicts()
+{
+	let r = capture_squarkup_from("errors/many-conflicts");
+	assert_contains!( r, "conflict" );
+	assert_contains!( r, "1.md" );
+	assert_contains!( r, "2.md" );
+	assert_contains!( r, "3.md" );
 }
