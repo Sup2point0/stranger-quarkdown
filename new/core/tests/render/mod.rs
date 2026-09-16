b6a767257f15dfaa4cefcf74df71f25ae2f0c86e
Sup#2.0 <sup2point0@gmail.com>
2026-09-16 22:01:10 +0100

    【core】repair tests

--- new/core/tests/render/mod.rs
+++ new/core/tests/render/mod.rs
@@ -14,9 +14,9 @@ use assertables::*;
 	let f3 = read_file("render/dest/nest/nest/3/+page.svx");
 	let f4 = read_file("render/dest/nest/nest/nest/4/+page.svx");
 	let f5 = read_file("render/dest/nest/nest/nest/nest/5/+page.svx");
-	assert_contains!( f1, "# 1" );
-	assert_contains!( f2, "# 2" );
-	assert_contains!( f3, "# 3" );
-	assert_contains!( f4, "# 4" );
-	assert_contains!( f5, "# 5" );
+	assert_contains!( f1, "One" );
+	assert_contains!( f2, "Two" );
+	assert_contains!( f3, "Three" );
+	assert_contains!( f4, "Four" );
+	assert_contains!( f5, "Five" );
 }
