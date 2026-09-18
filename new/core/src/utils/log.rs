fdae3668c14381037310f3cbfcca13081297f4e2
Sup#2.0 <sup2point0@gmail.com>
2026-09-18 16:16:00 +0100

    【core】use `format_args!()` over `format!()` in `log` to avoid unnecessary string allocation

--- new/core/src/utils/log.rs
+++ new/core/src/utils/log.rs
@@ -8,37 +8,37 @@ use std::fmt::Display;
 
 /// Log an action that Squarkdown is about to perform, to set up expectations.
 #[macro_export] macro_rules! is {
-	($msg:literal) => { $crate::log::log_is(format!($msg)) };
-	($var:expr)    => { $crate::log::log_is(format!("{}", $var)) };
-	($($args:tt)*) => { $crate::log::log_is(format!($($args)*)) };
+	($msg:literal) => { $crate::log::log_is(format_args!($msg)) };
+	($var:expr)    => { $crate::log::log_is(format_args!("{}", $var)) };
+	($($args:tt)*) => { $crate::log::log_is(format_args!($($args)*)) };
 } pub use is;
 
 /// Log a generic informative message with no special colouring, which can be quickly skimmed past.
 #[macro_export] macro_rules! info {
-	($msg:literal) => { $crate::log::log_info(format!($msg)) };
-	($var:expr)    => { $crate::log::log_info(format!("{}", $var)) };
-	($($args:tt)*) => { $crate::log::log_info(format!($($args)*)) };
+	($msg:literal) => { $crate::log::log_info(format_args!($msg)) };
+	($var:expr)    => { $crate::log::log_info(format_args!("{}", $var)) };
+	($($args:tt)*) => { $crate::log::log_info(format_args!($($args)*)) };
 } pub use info;
 
 /// Log a checkpoint that has successfully been reached.
 #[macro_export] macro_rules! ok {
-	($msg:literal) => { $crate::log::log_ok(format!($msg)) };
-	($var:expr)    => { $crate::log::log_ok(format!("{}", $var)) };
-	($($args:tt)*) => { $crate::log::log_ok(format!($($args)*)) };
+	($msg:literal) => { $crate::log::log_ok(format_args!($msg)) };
+	($var:expr)    => { $crate::log::log_ok(format_args!("{}", $var)) };
+	($($args:tt)*) => { $crate::log::log_ok(format_args!($($args)*)) };
 } pub use ok;
 
 /// Log an error, failure or issue the user should be aware of.
 #[macro_export] macro_rules! bad {
-	($msg:literal) => { $crate::log::log_bad(format!($msg)) };
-	($var:expr)    => { $crate::log::log_bad(format!("{}", $var)) };
-	($($args:tt)*) => { $crate::log::log_bad(format!($($args)*)) };
+	($msg:literal) => { $crate::log::log_bad(format_args!($msg)) };
+	($var:expr)    => { $crate::log::log_bad(format_args!("{}", $var)) };
+	($($args:tt)*) => { $crate::log::log_bad(format_args!($($args)*)) };
 } pub use bad;
 
 /// Log a hint to the user that may help them fix an error.
 #[macro_export] macro_rules! hint {
-	($msg:literal) => { $crate::log::log_hint(format!($msg)) };
-	($var:expr)    => { $crate::log::log_hint(format!("{}", $var)) };
-	($($args:tt)*) => { $crate::log::log_hint(format!($($args)*)) };
+	($msg:literal) => { $crate::log::log_hint(format_args!($msg)) };
+	($var:expr)    => { $crate::log::log_hint(format_args!("{}", $var)) };
+	($($args:tt)*) => { $crate::log::log_hint(format_args!($($args)*)) };
 } pub use hint;
 
 
