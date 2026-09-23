a31205a52a23cd2943c572dc601c33d6dd70913a
Sup#2.0 <sup2point0@gmail.com>
2026-09-23 11:23:49 +0100

    【render】display `RenderCtx`

--- new/core/src/renderer/ctx.rs
+++ new/core/src/renderer/ctx.rs
@@ -1,17 +1,31 @@
 use crate::types::ContextStack;
 
+use enum_display::EnumDisplay;
+
 
 #[allow(clippy::upper_case_acronyms)]
-#[derive(Clone, Debug, Default, PartialEq, Eq)]
+#[derive(Clone, Default, PartialEq, Eq, Debug, EnumDisplay)]
 pub enum RenderCtx
 {
 	#[default]
+	#[display("")]
 	MARKDOWN,
 
+	#[display("in comment")]
 	COMMENT,
+
+	#[display("in code block")]
 	CODE,
+
+	// TODO stringify with key
+
+	#[display("in #SQUARK leave")]
 	LEAVE { key: Option<String> },
+
+	#[display("in #SQUARK slash")]
 	SLASH { key: Option<String> },
+
+	#[display("in #SQUARK only")]
 	ONLY,
 }
 
