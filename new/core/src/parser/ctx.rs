99aa96cc972c1b9730b02274af68d46618766ae2
Sup#2.0 <sup2point0@gmail.com>
2026-09-25 12:22:38 +0100

    【core】wrap `ctx!` in IIFE

--- new/core/src/parser/ctx.rs
+++ new/core/src/parser/ctx.rs
@@ -33,7 +33,7 @@ macro_rules! ctx
 	($self:ident, $ctx:expr => $body:block) => {
 		{
 			$self.ctx.push($ctx);
-			let r = { $body };
+			let r = (|| { $body })();
 			$self.ctx.try_pop($ctx)?;
 			r
 		}
